d5e942d76e4ce6e8b544f11f671a6000a08c3cf6
ogulcanhgul <ogulcangul52@gmail.com>
2021-07-29 22:48:40 +0300

    Commit 3

--- switch_conf.py
+++ switch_conf.py
@@ -1,7 +1,18 @@
 from netmiko import ConnectHandler
+from fpdf import FPDF
 import getpass
 
-cihaz_puan = 100
+pdf = FPDF()
+
+pdf.add_page()
+
+pdf.set_font("Times", size = 14)
+
+pdf.set_text_color(230,0,0)
+pdf.cell(200, 10, txt = "SWITCH GUVENLIK RAPORU",ln = 1, align = 'C')
+pdf.set_text_color(0,0,0)
+
+pdf.set_font("Times", size = 15)
 
 
 def getpasswd():
@@ -41,6 +52,8 @@ def aaarulescheck():
     output = net_connect.send_command('show run tacacs')
     if str.__contains__(output, "Invalid"):
         tacacs = False
+
+
     else:
         tacacs = True
 
@@ -50,6 +63,7 @@ def aaarulescheck():
         radius = True
     else:
         radius = False
+
 
     output = net_connect.send_command('sho run | i sshkey')
     if(str.__contains__(output,"username "+username)):
@@ -61,15 +75,51 @@ def aaarulescheck():
         a = input("Cihaz üzerinde tacacs+ konfigurasyonu bulunmamaktadır. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2]")
         if a==str(1):
             do_tacacs_config()
+        else:
+            pdf.set_font("Times", size = 14)
+
+            pdf.set_text_color(96,205,222)
+            pdf.cell(200, 30, txt = "TACACS+ Konfigure Edilmemis",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 12)
+            pdf.set_text_color(0,0,0)
+
+            pdf.cell(200,10,txt = "TACACS+, Cisco NX-OS cihazlarinin, yonetim kullanicilarinin uzak bir AAA sunucusuna karsi kimlik",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "dogrulamasi için kullanabilecegi bir kimlik dogrulama protokolüdur. Guvenligi ihlal edilmis hesaplari",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "ve kotü niyetli etkinlikleri izlemeyi cok daha kolay hale getirir.",ln = 1, align = 'L')
+
 
     if(radius==False):
         a = input("Cihaz üzerinde radius konfigurasyonu bulunmamaktadır. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2]")
         if a==str(1):
             do_radius_config()
+        else:
+            pdf.set_font("Times", size = 14)
+
+            pdf.set_text_color(96,205,222)
+            pdf.cell(200, 30, txt = "RADIUS Konfigure Edilmemis",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 12)
+            pdf.set_text_color(0,0,0)
+
+            pdf.cell(200,10,txt = "RADIUS, Cisco NX-OS aygitlarinin yonetim kullanicilarinin uzak bir AAA sunucusuna karsi kimlik",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "dogrulamasi icin kullanabilecegi bir kimlik dogrulama protokoludur. Bu yonetim kullanicilari Cisco NX-OS",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "cihazina herhangi bir protokol uzerinden erisebilir ve bu arka uc kimlik dogrulamasini kullanabilir.",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "Ayrica, ele gecirilmis hesaplari ve kotu amacli etkinlikleri izlemeyi cok daha kolay hale getirir.",ln = 1, align = 'L')
+
     if(localssh==False):
         a = input("Cihaz üzerinde yerel ssh anahtarı konfigurasyonu bulunmamaktadır. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2]")
         if a==str(1):
             do_localssh_config()
+
+        else:
+            pdf.set_text_color(96,205,222)
+            pdf.set_font("Times", size = 14)
+            pdf.cell(200, 30, txt = "Yerel SSH Anahtari Konfigure Edilmemis",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 12)
+            pdf.set_text_color(0,0,0)
+
+            pdf.cell(200,10,txt = "SSH oturumlarini dogrulamak icin istemcinin ortak anahtarini kullanmak, anahtara yonetici",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "girisi icin parola kullanma ihtiyacini ortadan kaldirir.",ln = 1, align = 'L')
+
 
 def do_localssh_config():
 
@@ -133,6 +183,8 @@ def audit_for_lines():
     console = False
     sshses = False
     output = net_connect.send_command('sho run | section console')
+    a = ""
+    b = ""
     if(str.__contains__(output,"exec-timeout")):
         console = True
 
@@ -146,9 +198,23 @@ def audit_for_lines():
             do_console_timeout()
 
     if(sshses==False):
-        a = input("Cihaz üzerinde uzak bağlantılar için zaman aşımı konfigüre edilmemiş. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2] ")
-        if a==str(1):
+        b = input("Cihaz üzerinde uzak bağlantılar için zaman aşımı konfigüre edilmemiş. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2] ")
+        if b==str(1):
             do_ssh_timeout()
+
+    if (a != str(1)) or (b != str(1)):
+        pdf.set_text_color(96,205,222)
+        pdf.set_font("Times", size = 14)
+        pdf.cell(200, 30, txt = "Uzak Oturumlar Icin Zaman Asimi Konfigure Edilmemis",ln = 1, align = 'L')
+        pdf.set_font("Times", size = 12)
+        pdf.set_text_color(0,0,0)
+
+        pdf.cell(200,10,txt = "Bu konfigurasyon, yetkisiz kullanicilarin terk edilmis oturumlari kotüye kullanmasini onler. Örnegin, ag",ln = 1, align = 'L')
+        pdf.cell(200,10,txt = "yoneticisi gün icin ayrilirsa ve etkin bir oturum acma oturumuna sahip bir bilgisayari acik birakirsa",ln = 1, align = 'L')
+        pdf.cell(200,10,txt = "baska bir kullanici bu oturuma erisebilir. En iyi zaman asimi degerini belirlemek icin yerel  ",ln = 1, align = 'L')
+        pdf.cell(200,10,txt = "politikalarinizi ve operasyonel gereksinimlerinizi gozden gecirin. Cogu durumda, bu 10 dakikadan",ln = 1, align = 'L')
+        pdf.cell(200,10,txt = "fazla olmamalidir.",ln = 1, align = 'L')
+
 
 
 # ************************************************************TIMEOUT CONTROL END*****************************************************************************
@@ -159,7 +225,7 @@ def audit_for_lines():
 def do_vty_acl():
     name = input("Oluşturulacak acl için isim giriniz : ")
     remark= input("Oluşturulacak acl için açıklama giriniz : ")
-    permitted_hosts = input("Izin vereceginiz adresi [IP]/[MASK] şeklinde giriniz. (192.168.122.13/24) : ")
+    permitted_hosts = input("Izin vereceginiz adresi [IP]/[MASK] şeklinde giriniz. (192.1612.122.13/24) : ")
     config_commands = ['ip access-list '+str(name),'remark access-class '+str(remark),'permit ip '+str(permitted_hosts)+" any",'deny ip any any log']
     net_connect.send_config_set(config_commands)
     config_commands = ['line vty','access-class '+name+" in"]
@@ -178,6 +244,15 @@ def audit_for_vty_acl():
         a = input("Uzak terminal oturumları için access list konfigüre edilmemiş. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2] ")
         if a==str(1):
             do_vty_acl()
+        else:
+            pdf.set_text_color(96,205,222)
+            pdf.set_font("Times", size = 14)
+            pdf.cell(200, 30, txt = "Telnet Oturumlarina Erisim Kisitlanmamis",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 12)
+            pdf.set_text_color(0,0,0)
+
+            pdf.cell(200,10,txt = "Yonetim arayuzunu cok genis bir sekilde aciga cikarmak, bu arayuzu Mitm (ortadaki adam) saldirilarina",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "ve Kimlik Bilgisi doldurma saldirilarina maruz birakir.",ln = 1, align = 'L')
 
 
 # ************************************************************VTY ACCESS RESTRICTION END***********************************************************************
@@ -201,6 +276,16 @@ def audit_password_strenght():
         a = input("Güçlü şifre gereksinimleri konfigüre edilmemiş. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2] ")
         if a==str(1):
             do_password_strength()
+        else:
+            pdf.set_text_color(96,205,222)
+            pdf.set_font("Times", size = 14)
+            pdf.cell(200, 30, txt = "Guclu Sifre Gereksinimleri Etkinlestirilmemis",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 12)
+            pdf.set_text_color(0,0,0)
+
+            pdf.cell(200,10,txt = "Bir arka uc kimlik dogrulama yapilandirmak onerilen yapilandirma olsa da,en az bir yerel yonetim hesabi",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "yapilandirilmalidir. Bu nedenle, tum yerel yonetim hesaplari icin parola gucu icin minimum bir gereksinim",ln = 1, align = 'L')
+            pdf.cell(200,10,txt = "saglamak onemlidir.",ln = 1, align = 'L')
 
 
 def do_userdef_lifetime():
@@ -230,9 +315,21 @@ def audit_password_lifetime():
 
     output = net_connect.send_command('show userpassphrase timevalues')
     if str.__contains__(output, "99999"):
-        a = input("Her kullanici icin(Default) sifre ömrü konfigüre edilmemis. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2] ")
+        b = input("Her kullanici icin(Default) sifre ömrü konfigüre edilmemis. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2] ")
         if a==str(1):
             do_default_lifetime()
+
+    if(a!=str(1) or b!=str(1)):
+        pdf.set_text_color(96,205,222)
+        pdf.set_font("Times", size = 14)
+        pdf.cell(200, 30, txt = "Sifre Omru ve Uyari Etkinlestirilmemis",ln = 1, align = 'L')
+        pdf.set_font("Times", size = 12)
+        pdf.set_text_color(0,0,0)
+
+        pdf.cell(200,10,txt = "NX-OS, yerel kimlik bilgileri icin izin verilen parola omrünü ve son kullanma tarihinden once 'uyari süresini' ",ln = 1, align = 'L')
+        pdf.cell(200,10,txt = "ve son kullanma tarihinden sonra 'izin verilen giris gununu' ayarlamak icin komutlara sahiptir. Yerel kimlik",ln = 1, align = 'L')
+        pdf.cell(200,10,txt = "bilgileri kullaniliyorsa, bunlarin kurulusa uygun bir degere ayarlanmasi onerilir. Bu zamanlayicilar 'admin' kimlik",ln = 1, align = 'L')
+        pdf.cell(200,10,txt = "bilgisi icin ayarlanamaz.",ln = 1, align = 'L')
 
 # ************************************************************PASSWORD RULES END*******************************************************************************
 
@@ -292,6 +389,14 @@ def audit_logging():
         a = input("Kayıtlar(loglar) bir sunucuya gönderilmiyor. Sunucu konfigürasyonu yapmak ister misiniz? EVET[1] HAYIR[2] ")
         if a == str(1):
             do_log_server_conf()
+        else:
+            pdf.set_text_color(96,205,222)
+            pdf.set_font("Times", size = 14)
+            pdf.cell(200, 30, txt = "Loglama Duzgun Yapliandirilmamis",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 12)
+            pdf.set_text_color(0,0,0)
+
+            pdf.cell(200,10,txt = "Duzgun loglar cihazin gecmisini anlamaya ve bir sorun durumunda bir daha yasanmamasi icin ipuclari verir",ln = 1, align = 'L')
 
 
 # ************************************************************LOGGING RULES END**********************************************************************
@@ -363,6 +468,16 @@ def local_backup_audit():
             a = input("Yerel yedekleme konfigüre edilmemiş. Konfigüre etmek ister misiniz? EVET[1] HAYIR[2] ")
             if a == str(1):
                 do_local_backup()
+            else:
+                pdf.set_text_color(96,205,222)
+                pdf.set_font("Times", size = 14)
+                pdf.cell(200, 30, txt = "Duzenli Yedek Gorevi Yapilandirilmadi",ln = 1, align = 'L')
+                pdf.set_font("Times", size = 12)
+                pdf.set_text_color(0,0,0)
+
+                pdf.cell(200,10,txt = "Gecerli yedeklemelere sahip olmak, kullanicinin yapilandirma hatasi durumunda yapilandirmayi geri alabilecegi",ln = 1, align = 'L')
+                pdf.cell(200,10,txt = "bir ortam olusturur. Ayrica, bir ele gecirilme durumunda, yeni bir yedekleme, cihazi kisa bir sure",ln = 1, align = 'L')
+                pdf.cell(200,10,txt = "icinde calisma durumuna geri dondurebilir.",ln = 1, align = 'L')
 
 
 
@@ -399,6 +514,12 @@ def audit_empty_ports():
         a = input("Defaul vlan 1 de acik portlariniz bulunmakta. Baska vlan'e tasimak ister misiniz? EVET[1] HAYIR[2] ")
         if a ==str(1):
             do_move_ports()
+        else:
+            pdf.set_text_color(96,205,222)
+            pdf.set_font("Times", size = 11)
+            pdf.cell(200, 30, txt = "Default Vlan Kullanimi Varsayilan Olarak Bulunuyor",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 11)
+            pdf.set_text_color(0,0,0)
 
 
 def do_trunk_native():
@@ -437,6 +558,13 @@ def audit_trunk_natives():
     if a==str(1):
         do_trunk_native()
 
+    else:
+        pdf.set_text_color(96,205,222)
+        pdf.set_font("Times", size = 11)
+        pdf.cell(200, 30, txt = "Trunk Portlarda Native Vlan 1 Kullaniliyor",ln = 1, align = 'L')
+        pdf.set_font("Times", size = 11)
+        pdf.set_text_color(0,0,0)
+
 
 
 # ************************************************************VLAN SECURITY END****************************************************************
@@ -460,10 +588,43 @@ def audit_telnet():
         if a==str(1):
             do_disable_telnet()
         else:
-            pass
+            pdf.set_text_color(96,205,222)
+            pdf.set_font("Times", size = 14)
+            pdf.cell(200, 30, txt = "Telnet erisimine izin veriliyor",ln = 1, align = 'L')
+            pdf.set_font("Times", size = 12)
+            pdf.set_text_color(0,0,0)
+
+            pdf.cell(200,10,txt = "Telnet clear-text bir protokoldur ve guvenilir bir protokol olarak kabul edilmez.",ln = 1, align = 'L')
 
 
 # ************************************************************TELNET DISABLING END*********************************************************************
 
+
+
+aaarulescheck()
+
+audit_for_lines()
+
+audit_telnet()
+
+audit_for_vty_acl
+
+audit_password_strenght()
+
+audit_password_lifetime()
+
+audit_snmp_version()
+
+audit_logging()
+
+audit_dhcp_trust()
+
+local_backup_audit()
+
+audit_empty_ports()
+
+audit_trunk_natives()
+
+pdf.output("Cisco-Nx-Bnechmark-Result.pdf")
 
 
